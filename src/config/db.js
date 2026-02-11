98da2daf1b012d5fc8b1903099cea8c828a0123c
Sibusiso Msimango <smsimango@gmail..com>
2026-02-11 17:04:54 +0200

    Use Heroku DATABASE_URL

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,19 @@
 import pg from "pg";
+const { Pool } = pg;
 
-export const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "mms",
-    password: process.env.DB_PASSWORD,
-    port: 5432,
-})
+export const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
+});
+
+
+// export const db = new pg.Client({
+//     user: "postgres",
+//     host: "localhost",
+//     database: "mms",
+//     password: process.env.DB_PASSWORD,
+//     port: 5432,
+// })
 
 export async function connectDB() {
   await db.connect();
